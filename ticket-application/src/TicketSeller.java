958311cb6937804341b007e5126d4031005a609c
roach <tmdgusya@naver.com>
2021-08-17 06:44:36 +0900

    [chapter01] TicketOffice 의 dependency 증가로 Rollback

--- ticket-application/src/TicketSeller.java
+++ ticket-application/src/TicketSeller.java
@@ -7,6 +7,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        ticketOffice.sellTicketTo(audience);
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
